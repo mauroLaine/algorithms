53098105467aa99e0247d507c620b823ba3e2501
Mauricio Laine <laine.mau@gmail.com>
2021-03-10 22:41:41 -0800

    Created a more efficient algorithm for calculating the fibonacci number

--- w2/fibonacci.py
+++ w2/fibonacci.py
@@ -6,5 +6,17 @@ def calc_fib(n):
     return calc_fib(n - 1) + calc_fib(n - 2)
 
 
-n = int(input())
-print(calc_fib(n))
+def fib_list(n):
+    number_list = [None] * (n + 1)
+    number_list[0] = 0
+    number_list[1] = 1
+    for i in range(2, n + 1):
+        number_list[i] = number_list[i - 1] + number_list[i - 2]
+        # print(number_list)
+    return number_list[n]
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print(calc_fib(n))
+    print(fib_list(n))
